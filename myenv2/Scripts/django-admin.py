04df581a735f3ef9a075b80d318f66b3a17711e1
Gert Johannes Fourie <132646907+GJprocode@users.noreply.github.com>
2024-05-01 14:59:33 +0700

    new myenv2

---
+++ myenv2/Scripts/django-admin.py
@@ -0,0 +1,21 @@
+#!D:\Gert Files\My Career\Programming\Gert Python\Hyperion Dev\Levels\2 - Introduction to Software Engineering\L2T23\careershiftforum\myenv2\Scripts\python.exe
+# When the django-admin.py deprecation ends, remove this script.
+import warnings
+
+from django.core import management
+
+try:
+    from django.utils.deprecation import RemovedInDjango40Warning
+except ImportError:
+    raise ImportError(
+        'django-admin.py was deprecated in Django 3.1 and removed in Django '
+        '4.0. Please manually remove this script from your virtual environment '
+        'and use django-admin instead.'
+    )
+
+if __name__ == "__main__":
+    warnings.warn(
+        'django-admin.py is deprecated in favor of django-admin.',
+        RemovedInDjango40Warning,
+    )
+    management.execute_from_command_line()
